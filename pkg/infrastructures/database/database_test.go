a8c0a84a405f233ad8bd62d2582f7088ac3a55cc
20jun01 <10junjun20@gmail.com>
2023-04-22 18:15:56 +0900

    ✅ add temp test

---
+++ pkg/infrastructures/database/database_test.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"reflect"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewClientImpl(t *testing.T) {
+	tests := []struct {
+		name string
+		want *ClientImpl
+	}{
+		{
+			name: "success",
+			want: &ClientImpl{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewClientImpl()
+
+			assert.Equal(t, reflect.TypeOf(got), reflect.TypeOf(tt.want))
+		})
+	}
+}

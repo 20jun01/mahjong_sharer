98f4710ebb0670d44cd8e91adf74e5f1d5e9ef57
20jun01 <10junjun20@gmail.com>
2023-07-25 18:36:43 +0900

    ✨ intro sqlc

---
+++ app/go/db_config/schemas/user.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `password_hash` varchar(255) NOT NULL,
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
